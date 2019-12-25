f65d08110417b107dfa9c71a7f16cf5e44d8dc68
AndYankovich <andrei.yankovich.yrievich@gmail.com>
2019-12-25 19:57:01 +0300

    Updates

---
+++ precache-manifest.b07ea6b05ef363208c8cdc8970a49b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfea85472879d5ab903bb7213b205e6",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5749519b76187d24becb",
+    "url": "/Portfolio/static/css/main.b1f46907.chunk.css"
+  },
+  {
+    "revision": "82a6a1fd8921c4e6cf23",
+    "url": "/Portfolio/static/js/2.bce56c8e.chunk.js"
+  },
+  {
+    "revision": "5749519b76187d24becb",
+    "url": "/Portfolio/static/js/main.65f7c7ef.chunk.js"
+  },
+  {
+    "revision": "07a6b93e3f7a2363695c",
+    "url": "/Portfolio/static/js/runtime-main.500d6ffd.js"
+  }
+]);
